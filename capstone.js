2c8161f8073197d1ae8cf2b91ec0ed14fa359d03
cadencepatrick <89175478+cadencepatrick@users.noreply.github.com>
2024-12-04 00:12:01 -0500

    Update capstone.js

--- capstone.js
+++ capstone.js
@@ -3,7 +3,8 @@ console.log("Accessibility Rocks")
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
-
+document.body.classList.toggle("dark-theme");
+    
     var className = document.body.className;
     if(className.includes("light-theme")) {
         this.textContent = "Dark Mode";
